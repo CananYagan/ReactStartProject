f8a8eb1ef5d3747427e45d49685262e83e8d868a
Canan11 <cananyagan11@gmail.com>
2023-02-08 08:00:50 +0300

    PropTypes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import './App.css';
 import Header from './components/Header'
 import User from './components/User'
 
+const users=[
+  {id:1,name:"Canan"}
+  ,{id:2,name:"Halim"}]
+
 function App() {
   return (
     <>
       {/* jsx html yazar gibi javascipt yazma = return createElement("div",null,"hello")*/}
      <Header/>
-     <User name="Canan" surname="Yagan" age={30} isLoggedIn={true}/>
-     
+     <User 
+     name="Canan" 
+     surname="Yagan" 
+     age={"30"} 
+     isLoggedIn={true} 
+     friends={["Ayşe","Fatma","Zeynep"]}
+     //users={users}
+     />
+
     </>
   );
 }
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,13 +1,47 @@
+import PropTypes from "prop-types";
 
+function User({name,surname,age,isLoggedIn,friends,users}){
+    return (
+      <>
+        <h1>
+          {" "}
+          {isLoggedIn ? `${name} ${surname} ${age} ` : "Giriş Yapmadınız"}
+        </h1>
 
-function User({name,surname,age,isLoggedIn}){
-    console.log()
-    return(
-        <>
-            <h1> {isLoggedIn ? `${name} ${surname} ${age} `: "Giriş Yapmadınız"}</h1>
-          
-        </>
-    )
+        {friends.map((friends) => (
+          <div>{friends}</div>
+        ))}
+
+        {friends.map((friends, index) => (
+          <p key={index}>
+            {index}-{friends}
+          </p>
+        ))}
+
+        {/*Eğer içerisinde başka işlem de yapılacaksa return kullanılır
+         {users.map((users) => {
+          return (
+            <p key={users.id}>
+              {users.id} {users.name}
+            </p>
+          );
+        })} */}
+
+        {users && users.map((users) => (
+          <p key={users.id}>
+            {users.id}-{users.name}
+          </p>
+        ))}
+      </>
+    );
 }
+User.propTypes={
+  name:PropTypes.string.isRequired,
+  surname:PropTypes.string.isRequired,
+  isLoggedIn:PropTypes.bool.isRequired,
+  age:PropTypes.oneOfType([PropTypes.number,PropTypes.string]).isRequired,
+  friends:PropTypes.array.isRequired,
+  users:PropTypes.array
+};
 
 export default User;
